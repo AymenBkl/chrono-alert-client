66e39f394c963cf324fc404772aff8f2d1c50766
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-09 08:36:23 -0400

    faq added

--- src/app/pages/faq/faq-holder/faq-holder.component.ts
+++ src/app/pages/faq/faq-holder/faq-holder.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqHolderComponent implements OnInit {
 
+  faqs = [{
+    title:'Getting Started',
+    subTitle:'What is ChronoAlerts? Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor?',
+    innerTittle:'What is ChronoAlerts?',
+    innerContent:'Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor. Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor'
+  },
+  {
+    title:'Manage My Account',
+    subTitle:'Plans and Pricing Lorem ipsum dolor sit amet. Consectetur adipiscing elit.',
+    innerTittle:'Manage My Account',
+    innerContent:'Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor. Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor'
+  },
+  {
+    title:'Lorem ipsum',
+    subTitle:'Donec mauris tortor. Lorem ipsum dolor sit amet. Consectetur adipiscing elit.',
+    innerTittle:'Lorem ipsum',
+    innerContent:'Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor. Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor'
+  },
+  {
+    title:'Quick Links',
+    subTitle:'Get help signing in Lorem ipsum dolor sit amet. Consectetur adipiscing elit.',
+    innerTittle:'Quick Links',
+    innerContent:'Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor. Lorem ipsum dolor sit amet? Consectetur adipiscing elit? Donec mauris tortor'
+  }]
+
+  selectedFaq:any;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectFaq(faq) {
+    this.selectedFaq = faq;
+  }
+
+  unSelectFaq() {
+    this.selectedFaq = null;
   }
 
 }
