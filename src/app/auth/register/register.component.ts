60419da0d8463fbadb75298f5f4d350a9f11a207
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-09 13:18:07 -0400

    regiset values changes and reactive form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from './must-matchValdiator';
+import { onValueChanged } from './valueChanges';
 
 @Component({
   selector: 'app-register',
@@ -8,13 +11,60 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
   
   showPassword:boolean = false;
-  constructor() { }
+  registerForm: FormGroup;
+  formErrors: any;
+  submitted = false;
+  validationErrors: {errmsg , errcode};
+  hide = true;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildregisterForm();
+  }
+
+  buildregisterForm() {
+    this.registerForm = this.formBuilder.group({
+      email : [{value:'',disabled:this.submitted}, [Validators.required, Validators.email]],
+      password:[{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(6)]],
+      confirmPassword:[{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(6)]],
+      firstName : [{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(3),Validators.maxLength(20)]],
+      lastName : [{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(3),Validators.maxLength(20)]],
+      username : [{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(5),Validators.maxLength(20)]],
+      city : [{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(4),Validators.maxLength(20)]],
+      country : [{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(2),Validators.maxLength(20)]],
+      address : [{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(6),Validators.maxLength(20)]],
+      postalCode: [{value:'',disabled:this.submitted}, [Validators.required, Validators.minLength(5),Validators.maxLength(20)]],
+
+    },
+    {
+      validators : MustMatch('password', 'confirmPassword')
+    });
+    this.registerForm.valueChanges
+      .subscribe(user => {
+        this.formErrors = onValueChanged(user, this.registerForm);
+      });
   }
 
   showHidePassword(show:boolean) {
     this.showPassword = show;
   }
+
+  strOnlyValidation(event: any) {
+    const pattern = /[0-9]/;
+    let inputChar = String.fromCharCode(event.charCode);
+
+    if (!pattern.test(inputChar)) {
+      // invalid character, prevent input
+      event.preventDefault();
+    }
+    
+  }
+
+  omit_special_char(event)
+  {   
+   var k;  
+   k = event.charCode;  //         k = event.keyCode;  (Both can be used)
+   return((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || k == 32 || (k >= 48 && k <= 57)); 
+ }
 
 }
---
+++ src/app/auth/register/valueChanges.ts
@@ -0,0 +1,92 @@
+import { FormGroup } from '@angular/forms';
+
+// tslint:disable-next-line: prefer-const
+let formErrors = {
+    email: '',
+    password:'',
+    confirmPassword:'',
+    city:'',
+    country:'',
+    firstName:'',
+    lastName:'',
+    address:'',
+    postalCode:'',
+    username:''    
+};
+
+// tslint:disable-next-line: prefer-const
+let validationMessages = {
+    firstName: {
+        required: 'First Name is required',
+        minlength:'First Name must be at least 3 characters long.',
+        maxlength : 'First Name must be at most 20 characters long.'
+    },
+    lastName: {
+        required: 'Last Name is required',
+        minlength:'Last Name must be at least 3 characters long.',
+        maxlength : 'Last Name must be at most 20 characters long.'
+    },
+    address: {
+        required: 'Address is required',
+        minlength:'Address must be at least 6 characters long.',
+        maxlength : 'Address must be at most 20 characters long.'
+    },
+    username: {
+        required: 'User Name is required',
+        minlength:'User Name  must be at least 5 characters long.',
+        maxlength : 'User Name  must be at most 20 characters long.'
+    },
+    city: {
+        required: 'City is required',
+        minlength:'City must be at least 4 characters long.',
+        maxlength : 'City must be at most 20 characters long.'
+    },
+    postalCode : {
+        required: 'Postal Code is required',
+        minlength:'Postal Code must be at least 5 characters long.',
+        maxlength : 'Postal Code must be at most 20 characters long.',
+    },
+    country: {
+        required: 'Country is required',
+        minlength:'Country must be at least 2 characters long.',
+        maxlength : 'Country must be at most 20 characters long.'
+    },
+    email : {
+        required: 'Email is required',
+        email:'Please Enter A Valid Email'
+    },
+    password : {
+        required: 'Password is required.',
+        minlength: 'Password must be at least 6 characters long.',
+        mustMatch : 'Passwords do not match'
+    },
+    confirmPassword : {
+        required: 'Password is required',
+        minlength: 'Password must be at least 6 characters long.',
+        mustMatch : 'Passwords do not match'
+    }
+
+
+};
+
+export function onValueChanged(data: any, contactUsForm: FormGroup) {
+    console.log(contactUsForm);
+    if (!contactUsForm) { return; }
+    const form = contactUsForm;
+    for (const field in formErrors) {
+        if (formErrors.hasOwnProperty(field)) {
+            // clear previous error message (if any)
+            formErrors[field] = '';
+            const control = form.get(field);
+            if (control && control.dirty && !control.valid) {
+                const messages = validationMessages[field];
+                for (const key in control.errors) {
+                    if (control.errors.hasOwnProperty(key)) {
+                        formErrors[field] += messages[key] + ' ';
+                    }
+                }
+            }
+        }
+    }
+    return formErrors;
+}
