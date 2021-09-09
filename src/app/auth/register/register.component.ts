e6a9e33bd8e03b26ad723c814b18da2d824db553
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-09 10:28:25 -0400

    register added

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  
+  showPassword:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showHidePassword(show:boolean) {
+    this.showPassword = show;
   }
 
 }
