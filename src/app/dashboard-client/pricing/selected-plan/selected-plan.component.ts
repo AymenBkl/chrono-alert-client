42c5d8c7adb5d04c798cc2b561ba41e0859b07ab
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-12 16:06:50 -0400

    adding back button

--- src/app/dashboard-client/pricing/selected-plan/selected-plan.component.ts
+++ src/app/dashboard-client/pricing/selected-plan/selected-plan.component.ts
@@ -8,9 +8,35 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SelectedPlanComponent implements OnInit {
 
   @Input('plan') plan;
+  payments = [{
+    paymentType:'Paypal',
+    img:'../../../../assets/img/client-dashboard/paypal.svg',
+    selected:true
+  },
+  {
+    paymentType:'Credit Card',
+    img:'../../../../assets/img/client-dashboard/credit-card.svg',
+    selected:false
+  },
+  {
+    paymentType:'Lorem Ipsum',
+    img:'../../../../assets/img/client-dashboard/coin-stack.svg',
+    selected:false
+  },{
+    paymentType:'Lorem Ipsum',
+    img:'../../../../assets/img/client-dashboard/coin-stack.svg',
+    selected:false
+  }];
+  selectedPayment:any = this.payments[0];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectPayment(paymentToSelect){
+    this.payments.map(payment => payment.selected = false);
+    paymentToSelect.selected = true;
+    this.selectedPayment = paymentToSelect;
   }
 
 }
