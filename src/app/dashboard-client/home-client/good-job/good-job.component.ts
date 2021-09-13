c38e397c27d68cf73f8440cfca6b14c2189e2452
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-13 15:27:38 -0400

    add url

--- src/app/dashboard-client/home-client/good-job/good-job.component.ts
+++ src/app/dashboard-client/home-client/good-job/good-job.component.ts
@@ -1,3 +1,4 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { slideInOut } from '../../animations/slideIn';
 
@@ -15,6 +16,8 @@ export class GoodJobComponent implements OnInit {
   @Input('appliedFilter') appliedFilters;
   @Input('urlEmail') urlEmail;
   @Input('notificationFilter') notificationFilters;
+  @Input('minPriceValue') minPriceValue;
+  @Input('maxPriceValue') maxPriceValue;
   @Input('user') user;
   constructor() { }
 
@@ -22,12 +25,35 @@ export class GoodJobComponent implements OnInit {
     this.constructUrl();
   }
 
-  constructUrl(){
+  constructUrlV1(){
     console.log(this.alerts);
     console.log(this.appliedFilters);
     console.log(this.urlEmail);
     console.log(this.notificationFilters);
     console.log(this.user);
+  }
+
+  constructUrl() {
+    let params = new HttpParams();
+    params = params.set('sortorder','5');
+    params = params.set('dosearch','true');
+    params = params.set('redirectToSearchIndex',"true");
+    params = params.set('resultview','block');
+    params = params.set('pageSize','120');
+    params = params.set('maxAgeInDays','0');
+    params = params.set('currencyId',this.notificationFilters.currency.name);
+    params = params.set('priceFrom',this.minPriceValue.toString());
+    params = params.set('priceTo',this.maxPriceValue.toString());
+    params = params.set('showpage','1');   
+    this.appliedFilters.map(filter => {
+      params = params.append(`${filter.type}`,`${filter.id}`);
+    })
+    if (this.appliedFilters.length > 0){
+      console.log('https://www.chrono24.com/search/index.htm?' + params.toString())
+      //this.closeDialog({url:'https://www.chrono24.com/search/index.htm?' + params.toString(),filterData:this.constructNotificationFilters(), email:'aymenxyz6@gmail.com',user:'6115d35deb40681b395131b4'})
+    }
+    else {
+    }
   }
 
 }
