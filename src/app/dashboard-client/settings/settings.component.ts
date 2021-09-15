6f7c7b0985479a470ae469d199afc285916a3523
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-15 07:45:24 -0400

    setting added

--- src/app/dashboard-client/settings/settings.component.ts
+++ src/app/dashboard-client/settings/settings.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { slideInOut } from '../animations/slideIn';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
+  animations: [
+    slideInOut
+  ]
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  user:User;
+  constructor(private authService: AuthService ) { }
 
   ngOnInit(): void {
+    this.getUser();
   }
+
+  getUser(){
+    this.user = this.authService.user;
+  }
+
+
 
 }
