335d213fbd1ae9a1daa602fa7361ee3d6bf83833
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-15 10:22:03 -0400

    active telegram added

--- src/app/dashboard-client/settings/settings.component.ts
+++ src/app/dashboard-client/settings/settings.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { slideInOut } from '../animations/slideIn';
+import { Plan } from '../interfaces';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -16,10 +18,17 @@ export class SettingsComponent implements OnInit {
 
   user:User;
   modalToShow:string = '';
-  constructor(private authService: AuthService ) { }
+  userPlan:string = 'Free';
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.getUser();
+    this.checkUserPlan();
+  }
+
+  goToPricing(){
+    this.router.navigate(['/dashboard-client/pricing']);
   }
 
   showModal(modalToShow:string){
@@ -32,6 +41,27 @@ export class SettingsComponent implements OnInit {
 
   getUser(){
     this.user = this.authService.user;
+  }
+
+  checkUserPlan(){
+    if (this.authService.user.plan && this.authService.user.plan != null){
+      var userPlan:Plan = this.authService.user.plan;
+      var date = new Date().getTime();
+      var monthDate = new Date(userPlan.expires).getTime();
+      if ((userPlan && userPlan.name == 'Pro' && monthDate - date < 30*24*60*60*1000)){
+        this.userPlan = 'Pro';
+      }
+      else if (userPlan && userPlan.name == 'Standard' && monthDate - date < 30*24*60*60*1000){
+        this.userPlan = 'Standard';
+      }
+      else {
+        this.userPlan = 'Free';
+      }
+    }
+    else {
+      this.userPlan = 'Free';
+    }
+    this.userPlan = 'Standard'
   }
 
 
