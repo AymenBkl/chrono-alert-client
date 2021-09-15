0e97a7e28908a331c89b03bc301e69f69caeaf62
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-15 09:00:05 -0400

    next slide

--- src/app/dashboard-client/instructions/telegram/telegram.component.ts
+++ src/app/dashboard-client/instructions/telegram/telegram.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter, Input } from '@angular/core';
 import { slideInOut } from '../../animations/slideIn';
 
 @Component({
@@ -12,10 +12,12 @@ import { slideInOut } from '../../animations/slideIn';
 export class TelegramComponent implements OnInit {
   @Output('telegramInstructionNext') telegramInstructionNext: EventEmitter<any> = new EventEmitter;
   @Output('telegramInstructionPrev') telegramInstructionPrev: EventEmitter<any> = new EventEmitter;
+  @Input('modal') modal;
   viewInstructions:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.modal);
   }
 
 
