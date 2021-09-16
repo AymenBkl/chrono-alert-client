fab0c391daf19066417518def109555efe3b7b57
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-16 14:11:54 -0400

    footer added

--- src/app/components/footer/footer-client/footer-client.component.ts
+++ src/app/components/footer/footer-client/footer-client.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer-client.component.scss']
 })
 export class FooterClientComponent implements OnInit {
+  year= new Date().getFullYear();
 
   constructor() { }
 
