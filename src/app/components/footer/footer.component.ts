f7325f60f65a7006ba1fa75dbedafebf940d32e2
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-16 13:17:40 -0400

    footer client fixed

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   year= new Date().getFullYear();
-  
-  constructor() { }
+  isLoggedIn:boolean = false;
+  subscriberRouter:boolean = false;
+  constructor(private authService: AuthService,
+              private router:Router) { }
 
   ngOnInit() {
+    this.routerChanges();
+  }
+
+  routerChanges(){
+    if (!this.subscriberRouter){
+      this.checkUser();
+      this.router.events.subscribe((val) => {
+        if (val instanceof NavigationEnd){
+          this.checkUser();
+          this.subscriberRouter = true;
+
+        }
+    });
+    }
+  }
+
+  checkUser(){
+    this.isLoggedIn = this.authService.isAuthenticated;
+    console.log(this.isLoggedIn,this.authService.isAuthenticated);
   }
 
   
--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -5,10 +5,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './footer.component';
 import { SubscriberFormComponent } from './subscriber-form/subscriber-form.component';
 import { SentSubscribeComponent } from './sent-subscribe/sent-subscribe.component';
+import { FooterClientComponent } from './footer-client/footer-client.component';
 
 @NgModule({
     imports: [ RouterModule, CommonModule, ReactiveFormsModule],
-    declarations: [ FooterComponent, SubscriberFormComponent, SentSubscribeComponent ],
+    declarations: [ FooterComponent, SubscriberFormComponent, SentSubscribeComponent, FooterClientComponent ],
     exports: [ FooterComponent ]
 })
 
