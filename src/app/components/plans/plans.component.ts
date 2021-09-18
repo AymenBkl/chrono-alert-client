7e62564ee7c51ed971ea30157efde6e1e9a94e7a
Aymen Abdelmoumen Boukhalkhel <a.boukhalkhel@esi-sba.dz>
2021-09-17 23:15:37 -0400

    plan added successfully

--- src/app/components/plans/plans.component.ts
+++ src/app/components/plans/plans.component.ts
@@ -1,20 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-plans',
   templateUrl: './plans.component.html',
   styleUrls: ['./plans.component.scss']
 })
-export class PlansComponent implements OnInit {
+export class PlansComponent implements OnInit,AfterViewInit {
 
   selectedPlan = 1;
+  innerWidth:any;
   constructor() { }
+ 
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+  this.innerWidth = window.innerWidth;
+  }
+
   selectPlan(index) {
-    this.selectedPlan = index;
+    if (this.innerWidth > 481){
+      this.selectedPlan = index;
+    }
+  }
+
+  nextPlan(){
+    if (this.innerWidth <= 481){
+      console.log(this.selectedPlan)
+      if (this.selectedPlan >= 1 && this.selectedPlan < 3) {
+        this.selectedPlan += 1;
+        console.log(this.selectedPlan)
+      }
+    }
+    
+  }
+
+  prevPlan(){
+    if (this.innerWidth < 481){
+      if (this.selectedPlan > 1 && this.selectedPlan <= 3) {
+        this.selectedPlan -= 1;
+      }
+    }
+    
   }
 
 }
